ab06d6949cd8ab2486182e9a99d06f418fb4f1bd
Joke Heyndels <jo_hannah@outlook.com>
2021-05-06 13:59:30 +0000

    follow tutorial up to dealing and saving cards, Throw error if no cards left

---
+++ cards/src/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class Card extends Component {
+    render(){
+        return(
+            <div>Card Component - Not yet used</div>
+        )
+        
+    }
+}
+
+export default Card
---
+++ cards/src/CardDealer.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react"
+import Card from "./Card"
+import axios from "axios"
+
+const API_URL = "https://deckofcardsapi.com/api/deck"
+
+class CardDealer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {deck: null, drawn: []}
+        this.deal = this.deal.bind(this)
+    }
+    async componentDidMount() {
+        console.log("COMPONENT DID MOUNT, UPDATE STATE")
+        let deck = axios.get(`${API_URL}/new/shuffle/`)
+        let data = (await deck).data
+        this.setState({ deck: data})
+    }
+
+    async deal() {
+        // make request using deck
+        try {
+            let cardRes = await axios.get(`${API_URL}/${this.state.deck.deck_id}/draw/`)
+            if (cardRes.data.remaining === 0) {
+                throw new Error("No cards remaining")
+            }
+            let card = cardRes.data.cards[0]
+            this.setState(st => ({
+                drawn: [
+                    ...st.drawn, 
+                    {
+                        id: card.code, 
+                        image: card.image, 
+                        name: `${card.value} of ${card.suit}`
+                    }
+                ]
+            }))
+        } catch (err) {
+            alert(err);
+        }
+    }
+
+    render() {
+        return(
+            <div>
+            <h1>Card Dealer</h1>
+            <button onClick={this.deal}>Deal me a card!</button>
+            <Card />
+        </div>
+        )
+        
+    }
+}
+
+export default CardDealer
